64dffb38485ae2c7bc1a25cb08394644f9d728a0
Brandy Webster <mathmatomancer@gmail.com>
2015-08-09 08:45:00 -0700

    adding in sidewinder

--- app/controllers/binary_maze_controller.rb
+++ app/controllers/binary_maze_controller.rb
@@ -3,7 +3,7 @@ class BinaryMazeController < ApplicationController
   def index
     size = [params[:size].to_i, 3].max
     @grid = Grid.new(size, size)
-    BinaryTree.on(@grid)
+    Sidewinder.on(@grid)
   end
   
 end
